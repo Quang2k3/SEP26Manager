ec113b097c29178ff9b01b96eeaa6586a8aacdae
HOANGNGAIIU\Admin <tranhoang1112003@gmail.com>
2026-01-27 17:24:58 +0700

    feat(exception): add global exception handling and error responses

---
+++ src/main/java/org/example/sep26management/infrastructure/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package org.example.sep26management.infrastructure.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
