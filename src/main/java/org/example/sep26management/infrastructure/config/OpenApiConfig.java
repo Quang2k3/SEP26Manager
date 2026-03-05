ac4733b320008f49b479cbd3ec8883d5d71d4788
Nguyễn Phước Quang <nguyen.quang6633@gmail.com>
2026-03-05 23:07:10 +0700

    fix: fix CORS

--- src/main/java/org/example/sep26management/infrastructure/config/OpenApiConfig.java
+++ src/main/java/org/example/sep26management/infrastructure/config/OpenApiConfig.java
@@ -26,8 +26,8 @@ public class OpenApiConfig {
                 final String securitySchemeName = "bearerAuth";
 
                 List<Server> servers = new ArrayList<>();
-                servers.add(new Server().url("http://localhost:8080/api").description("Local Development"));
-                servers.add(new Server().url("https://api.cleanhousewms.id.vn/api").description("Production"));
+                servers.add(new Server().url("http://localhost:8080").description("Local Development"));
+                servers.add(new Server().url("https://api.cleanhousewms.id.vn").description("Production"));
 
                 return new OpenAPI()
                                 .servers(servers)
