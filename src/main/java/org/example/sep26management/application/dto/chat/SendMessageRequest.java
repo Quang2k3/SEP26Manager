d2e5a07457b93134c7165dfa7bdbbe8f22355ac0
hoangthhe171527 <hoangthhe171527@fpt.edu.vn>
2026-03-27 10:24:05 +0700

    del : chat feature & voice

--- src/main/java/org/example/sep26management/application/dto/chat/SendMessageRequest.java
+++
@@ -1,8 +0,0 @@
-package org.example.sep26management.application.dto.chat;
-
-import lombok.*;
-
-@Data @NoArgsConstructor @AllArgsConstructor
-public class SendMessageRequest {
-    private String content;
-}
